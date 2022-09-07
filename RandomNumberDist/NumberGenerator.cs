d6eeb51bb8cb685ad9e88e19fa336e9d409712b7
David Catling <davecatling@gmail.com>
2022-09-07 21:05:54 +1000

    Solution rename

---
+++ RandomNumberDist/NumberGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenfordLawChecker
+{
+    internal class NumberGenerator
+    {
+        private Random generator = new Random();
+
+        public  List<byte[]> RandomValues(int quantity, byte length)
+        {
+            if (length < 2 || length > 19) throw new ArgumentException("Minumum length is 2, maximum is 19");
+            var min = (long)Math.Pow(10, length - 1);
+            var max = (long)Math.Pow(10, length) - 1;
+            var randomNumbers = new List<ulong>();
+            for (var numbersMade = 0; numbersMade < quantity; numbersMade++)
+                randomNumbers.Add((ulong)generator.NextInt64(min, max));
+            var result = new List<byte[]>();
+            randomNumbers.ForEach(rn => result.Add(UlongToByteArray(rn)));
+            return result;
+        }
+
+        private static byte[] UlongToByteArray(ulong value)
+        {
+            var strVal = value.ToString();
+            var result = new byte[strVal.Length];
+            for (var i = 0; i < strVal.Length; i++)
+                result[i] = byte.Parse(strVal.Substring(i, 1));
+            return result;
+        }
+    }
+}
---
+++ RandomNumberDist/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+namespace BenfordLawChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var exit = false;
+            while (!exit)
+            {
+                var numberGenerator = new NumberGenerator();
+                int? quantityToGenerate = null;
+                while (quantityToGenerate == null)
+                    quantityToGenerate = UserProvidedInt("Enter the quantity of numbers to generate:");
+                int? numberLength = null;
+                while (numberLength == null)
+                    numberLength = UserProvidedInt("Enter the length of each number (2 to 19):");
+                var randomNumbers = numberGenerator.RandomValues((int)quantityToGenerate, (byte)numberLength);
+                for (int i = 0; i < 10; i++)
+                    Console.WriteLine(ReportNumber(i, ref randomNumbers));
+                Console.WriteLine("Press enter to restart, any other key to exit");
+                var key = Console.ReadKey();
+                if (key.Key != ConsoleKey.Enter) exit = true;
+            }
+        }
+
+        private static string ReportNumber(int numberToReport, ref List<byte[]> randomNumbers)
+        {
+            var length = randomNumbers[0].Length;
+            var sb = new StringBuilder();
+            sb.AppendLine($"Distribution for number {numberToReport}:");
+            for (int i = 0; i < length; i++)
+            {
+                var percentage = PercentageInPosition(numberToReport, i, ref randomNumbers);
+                sb.Append($"{i + 1}: {percentage}% ");
+            }
+            return $"{sb}\r\n";
+        }
+
+        private static int? UserProvidedInt(string prompt)
+        {
+            Console.WriteLine(prompt);
+            var strValue = Console.ReadLine();
+            if (!int.TryParse(strValue, out int intValue))
+            {
+                Console.WriteLine($"[{strValue}] could not be interpreted as an integer");
+                return null;
+            }
+            return intValue;
+        }
+
+        private static float PercentageInPosition(int number, int posn, ref List<byte[]> randomValues)
+        {
+            var count = (float)(randomValues.Count(rv => rv[posn] == number));
+            float result = (count / randomValues.Count) * 100;
+            return result;
+        }
+    }
+}
